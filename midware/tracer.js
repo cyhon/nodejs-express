c08604650b38603d193a733a00ea49348539776b
linhaitao <linhaitao@finogeeks.com>
2018-01-11 12:13:09 +0800

    rename

--- midware/tracer.js
+++ midware/tracer.js
@@ -4,7 +4,7 @@ const TRACE_ID = 'x-trace-id';
 
 module.exports = function (req, res, next) {
     const traceId = req.header(TRACE_ID) || uuid();
-    req[TRACE_ID] = traceId;
+    req.traceId = traceId;
     res.setHeader(TRACE_ID, traceId);
     next();
 };
